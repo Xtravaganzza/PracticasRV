c01a0be59cdd5f3489c170b077ff4d376052fc3e
Xtravaganzza <xtravaganzza.17@gmail.com>
2022-03-29 23:54:18 -0600

    Practica5

---
+++ Practica5/Assets/Script/Controlador.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    public float vel = 10; //Para la velocidad del personaje
+    public  Rigidbody mirigidbody; //Para poder utilisar el RigidBody
+    // Start is called before the first frame update
+    void Start()
+    {
+        mirigidbody = GetComponent<Rigidbody>(); //Hacer una llamada al rigidBody
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Declarar variables para el movimiento mediante las teclas
+        float vercital;
+        float horizontal;
+        vercital = Imput.GetAxis("Vertical");
+        horizontal = Imput.GetAxis("Horizontal");
+
+        mirigidbody.AddForce(new Vector3(horizontal,0, vercital) *vel); //Para el movimiento
+    }
+    void OnTrigerEnter(Collider otro)
+    {
+        if (otro.CompareTag("Start"))
+        {
+            //Aqui se inicializa el cronometro
+            Debug.Log("has iniciado el laberinto");
+        }
+        if (otro.CompareTag("Finish"))
+        {
+            //Aqui se Finaliza el cronometro el cronometro
+            Debug.Log("has terminado con éxito");
+        }
+    }
+}
---
+++ Practica5/Assets/Script/ControladorCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCam : MonoBehaviour
+{
+    public GameObject jugador; //Referencia al jugador
+    Vector3 distancia; //Relacion de la distancia entre camara jugador
+    // Start is called before the first frame update
+    void Start()
+    {
+        distancia = transform.position - jugador.transform.position;   //Ver la distancia
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jugador.transform.position + distancia; //Sirve para seguir al jugador en caa update
+    }
+}
